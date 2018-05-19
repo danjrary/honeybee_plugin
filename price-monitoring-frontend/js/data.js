bef204db7f7221640e0b16f6aee0b32dd6af994c
danjrary <danjrary@gmail.com>
2018-05-19 15:07:28 +0800

    Add files via upload

---
+++ price-monitoring-frontend/js/data.js
@@ -0,0 +1,11 @@
+var jcontent = {
+	"name" : "John",
+	"price" : 300
+}
+
+var output = document.getElementById('output');
+
+output.innerHTML = jcontent.name + ' '
+
+
+// [ {"name": "吹風機","price": 300}, {"name": "麥克筆","price": 9000}, {"name": "筆記型電腦","price": 54555}, {"name": "Iphone 9","price": 32000}, {"name": "神奇海螺","price": 5000}, {"name": "PSP1007","price": 2000}, {"name": "鍵盤","price": 3500} ]
---
+++ price-monitoring-frontend/js/index.js
@@ -0,0 +1,209 @@
+const serverAddress = "http://104.131.165.92:8081"
+var productDetail;
+document.addEventListener('DOMContentLoaded', function() {
+
+  // When price condition is changed
+  $('select#condition').on('change', function() {
+    // If the user has selected "Less than current price"
+    // We assign greaterThanInput value to  0
+    // and set the lessThanInput value to current price of the product
+    if ($('#condition').val() == "lessThanCurrent") {
+      $('#greaterThanInput').val(0);
+      $('#lessThanInput').val(productDetail.price);
+      $('#greaterThanInput').attr('readonly', 'true');
+    }
+    // If the user has selected "Fixed price"
+    // We assign greaterThanInput value to be price entered by user
+    // and set the lessThanInput value to be price entered by user
+    else if ($('#condition').val() == "fixedPrice") {
+      $('#greaterThanInput').val(productDetail.price);
+      $('#greaterThanInput').val(productDetail.price);
+      $('#greaterThanInput').removeAttr('readonly');
+    }
+    // If the user has selected "Less than a value"
+    // We assign greaterThanInput value to 0
+    // and set the lessThanInput value to be price entered by user
+    else {
+      $('#greaterThanInput').val(0);
+      $('#greaterThanInput').removeAttr('readonly');
+    }
+  });
+
+  // On clicking the set alert button, we make a call to our server
+  // Sever indexes the product and set a trigger to send mail when condition is met
+  $('#submit').on('click', function() {
+    var parameters = {
+      'lte': $('#lessThanInput').val(),
+      'gte': $('#greaterThanInput').val(),
+      'email': $('#email').val(),
+      'productId': productDetail.productId
+    }
+    $.ajax({
+      type: 'GET',
+      url: serverAddress + '/alert',
+      data: parameters,
+      success: function(d) {
+        console.log(d);
+      },
+      error: function() {
+        console.log("error");
+      }
+    });
+    alert("You will receive an email, whenever the product price reaches according to you condition.");
+  });
+
+  $(document).ready(function() {
+    // Get the current tab URL and fetch the product ID
+    getCurrentTabUrl(function(url) {
+      if ($.urlParam('pid', url) != null) {
+        // Fetch the product details from the product ID
+        getProductDetails({ 'productId': $.urlParam('pid', url) }, function(data) {
+          //  Display the product details in the extension
+          productDetail = data;
+          $('#name').text(productDetail.name);
+          $('#current_price').text(productDetail.price);
+          var imageURL;
+          for (var key in productDetail.imageurls) {
+            imageURL = productDetail.imageurls[key];
+          }
+          $('#img').attr('src', imageURL);
+        });
+      }
+    });
+  });
+
+});
+
+
+
+
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+}
+
+
+
+$.urlParam = function(name,url){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(url);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
+
+
+function getProductDetails(parameters, callback) {
+  $.ajax({
+    type: 'get',
+    url: serverAddress + '/product',
+    data: parameters,
+    success: function(d) {
+      callback(d);
+    },
+    error: function() {
+      console.log("error");
+    }
+  });
+}
+
+
+
+
+
+var apiurl={
+  notifydata:"https://awiclass.monoame.com/api/command.php?type=get&name=notifydata",
+  itemdata:"https://awiclass.monoame.com/api/command.php?type=get&name=itemdata",
+  hahowdata:"https://awiclass.monoame.com/api/command.php?type=get&name=hahowdata"
+}
+
+// notify
+var vm1 = new Vue({
+  el: "#app1",
+  data: {
+    notifydata: "載入中"
+  },
+  mounted: function(){
+    $.ajax({
+      url: apiurl.notifydata,
+      success: function(res){
+        vm1.notifydata = res;
+        }
+      }
+    )
+  }
+})
+
+// 課程中使用ready代表: 虛擬機器(vm)準備好的時候，執行把資料載入；不過在Vue 2.x版本，ready要改為mounted
+
+// items
+var vm2 = new Vue({
+  el: "#app2",
+  data: {
+    items: []
+  },
+  mounted: function(){
+    $.ajax({
+      url: apiurl.itemdata,
+      success: function(res){
+        vm2.items = JSON.parse(res)
+      }
+    })
+  }
+})
+
+// 由於itemdata是由陣列［］組成，所以要先用JSON.parse轉換為JSON物件之後，才可以使用
+
+var vm3 = new Vue({
+  el: "#app3",
+  data: {
+    classes: []
+  },
+  mounted: function(){
+    $.ajax({
+      url: apiurl.hahowdata,
+      success: function(res){
+        vm3.classes = JSON.parse(res);
+      }
+    })
+  }
+  
+})
+
+
+
+var jcontent = {
+  "name" : "John",
+  "price" : 300
+}
+
+var output1 = document.getElementById('output1');
+output1.innerHTML = jcontent.name;
